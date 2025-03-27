9c245526783a113e4bd2e2ae6bcd5cb702d3f4e4
davidmonterrosa <dmonterrosa99@gmail.com>
2025-03-27 16:25:49 -0700

    Added interface and services to start connecting backend and frontend

--- src/utils/Interfaces/UserInterfaces.ts
+++ src/utils/Interfaces/UserInterfaces.ts
@@ -1,4 +1,10 @@
 
 export interface IUserCredentials {
-    
+    username: string
+    password: string
+}
+
+export interface IUserNameId {
+    id: number
+    username: string
 }
---
+++ src/utils/Services/DataServices.ts
@@ -0,0 +1,43 @@
+import { IUserCredentials, IUserNameId } from "../Interfaces/UserInterfaces";
+
+const url = "https://studybuddies-g9bmedddeah6aqe7.westus-01.azurewebsites.net/";
+
+let userCredentials: IUserCredentials;
+
+export const createAccount = async (user: IUserNameId) => {
+    const response: Response = await fetch(`${url}/User/register`, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body:JSON.stringify(user)
+    });
+
+    if(!response.ok) {
+        const data = await response.json();
+        console.log("Response is not ok");
+        return data.success;
+    }
+
+    const data = await response.json();
+    return data.success;
+}
+
+export const login = async (user: IUserNameId) => {
+    const response: Response = await fetch(`${url}/User/login`, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body:JSON.stringify(user)
+    });
+
+    if(!response.ok) {
+        const data = await response.json();
+        console.log("Log in failed: Username or password doesn't match system");
+        return null;
+    }
+
+    const data = await response.json();
+    return data;
+}
