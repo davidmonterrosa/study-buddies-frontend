ab9492ae87b7a269bf508f84feaada623e28abbb
AlanBatres1 <alanbatres328@gmail.com>
2025-04-01 10:19:22 -0700

    svg

--- src/app/(pages)/landing/page.tsx
+++ src/app/(pages)/landing/page.tsx
@@ -1,3 +1,4 @@
+import CommunityCard from '@/components/CommunityCard'
 import NavBarComponent from '@/components/NavBarComponent'
 import React from 'react'
 
@@ -6,6 +7,7 @@ const Dashboard = () => {
       <div className='bg-white'>
         <NavBarComponent/>
         <h1 className='text-black'> Welcome to the Dashboard</h1>
+        <CommunityCard/>
     </div>
   )
 }
