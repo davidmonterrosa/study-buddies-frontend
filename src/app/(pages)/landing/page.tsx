e7b8582105367174898c9d7c42a39329fb0fe2d5
davidmonterrosa <dmonterrosa99@gmail.com>
2025-03-31 23:56:00 -0700

    Added shadcn sidebar component

--- src/app/(pages)/landing/page.tsx
+++ src/app/(pages)/landing/page.tsx
@@ -1,8 +1,10 @@
+import NavBarComponent from '@/components/NavBarComponent'
 import React from 'react'
 
 const Dashboard = () => {
   return (
-    <div className='bg-white'>
+      <div className='bg-white'>
+        <NavBarComponent/>
         <h1 className='text-black'> Welcome to the Dashboard</h1>
     </div>
   )
