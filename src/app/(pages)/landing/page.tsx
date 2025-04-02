66172c232324b5622caedb5dd7665006940def52
davidmonterrosa <dmonterrosa99@gmail.com>
2025-04-02 15:57:23 -0700

    Created My Communities panel and added changes to dashboard page layout

--- src/app/(pages)/landing/page.tsx
+++ src/app/(pages)/landing/page.tsx
@@ -1,4 +1,5 @@
 import CommunityCard from '@/components/CommunityCard'
+import MyCommunitiesPanel from '@/components/MyCommunitiesPanel'
 import NavBarComponent from '@/components/NavBarComponent'
 import React from 'react'
 
@@ -6,44 +7,47 @@ const Dashboard: React.FC = () => {
   return (
     <div className='bg-white'>
       <h1 className='text-black'> Welcome to the Dashboard</h1>
-      <div className='w-full bg-white rounded-lg dark:bg-linear-to-b dark:from-[#271E55] dark:to-[#100B28] dark:border-[2px] dark:border-[#aa7dfc40] max-w-full lg:max-w-[80%] h-auto grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 grid-rows-4 gap-4 2xl:grid-cols-4 drop-shadow-[0_3px_4px_rgba(0,0,0,0.25)] p-4'>
-        <CommunityCard 
-          communityName="Spanish"
-          subject="Language"
-          buddies={121}
-          difficulty="Beginner"
-          initials="BB"
-          userName="Bea Boeteng"
-          isPublic={true}
-        />
-        <CommunityCard 
-          communityName="French"
-          subject="Language"
-          buddies={98}
-          difficulty="Intermediate"
-          initials="FB"
-          userName="François Boucher"
-          isPublic={false}
-        />
-        <CommunityCard 
-          communityName="German"
-          subject="Language"
-          buddies={50}
-          difficulty="Advanced"
-          initials="GB"
-          userName="Gerhard Becker"
-          isPublic={true}
-        />
-        <CommunityCard 
-          communityName="Italian"
-          subject="Language"
-          buddies={75}
-          difficulty="Beginner"
-          initials="IB"
-          userName="Irene Bellini"
-          isPublic={false}
-        />
-      </div>
+      <section className='flex flex-row gap-6 m-6'>
+        <MyCommunitiesPanel/>
+        <div className='w-full bg-white rounded-lg dark:bg-linear-to-b dark:from-[#271E55] dark:to-[#100B28] dark:border-[2px] dark:border-[#aa7dfc40] max-w-full lg:max-w-[80%] h-auto grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 grid-rows-4 gap-4 2xl:grid-cols-4 drop-shadow-[0_3px_4px_rgba(0,0,0,0.25)] p-4'>
+          <CommunityCard 
+            communityName="Spanish"
+            subject="Language"
+            buddies={121}
+            difficulty="Beginner"
+            initials="BB"
+            userName="Bea Boeteng"
+            isPublic={true}
+          />
+          <CommunityCard 
+            communityName="French"
+            subject="Language"
+            buddies={98}
+            difficulty="Intermediate"
+            initials="FB"
+            userName="François Boucher"
+            isPublic={false}
+          />
+          <CommunityCard 
+            communityName="German"
+            subject="Language"
+            buddies={50}
+            difficulty="Advanced"
+            initials="GB"
+            userName="Gerhard Becker"
+            isPublic={true}
+          />
+          <CommunityCard 
+            communityName="Italian"
+            subject="Language"
+            buddies={75}
+            difficulty="Beginner"
+            initials="IB"
+            userName="Irene Bellini"
+            isPublic={false}
+          />
+        </div>
+      </section>
     </div>
   )
 }
---
+++ src/components/MyCommunitiesPanel.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const MyCommunitiesPanel = () => {
+    const navigateToCommunity = () =>{
+
+    }
+
+
+  return (
+    <main className='z-50 flex flex-col w-1/5 h-auto  shadow-[0_0px_5px_rgba(0,0,0,0.25)] rounded-lg p-4'>
+        <h1 className='text-center text-2xl font-bold m-4'>My Communities</h1>
+        <div>
+            <button className='w-full rounded-2xl hover:bg-[#818cf88e] hover:cursor-pointer p-4' >
+                <p className='font-bold text-left'>Spanish Learning</p>
+            </button>
+            <button></button>
+        </div>
+    </main>
+  )
+}
+
+export default MyCommunitiesPanel
