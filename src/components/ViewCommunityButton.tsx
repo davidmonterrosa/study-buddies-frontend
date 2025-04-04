0b94dcd618a723c8084ed84edfd1cee88a4b9446
AlanBatres1 <alanbatres328@gmail.com>
2025-04-04 11:57:45 -0700

    hover effects on view community component

--- src/components/ViewCommunityButton.tsx
+++ src/components/ViewCommunityButton.tsx
@@ -14,12 +14,11 @@ const ViewCommunityButton: React.FC<ViewCommunityButtonProps> = ({
   return (
     <button
       onClick={onClick}
-      className={`w-full rounded-2xl p-4 hover:cursor-pointer dark:hover:shadow-[0_2px_15px_#6f58da] transition duration-200
-       ${
-         isActive
-           ? "bg-[#6f58da] text-white"
-           : "bg-transparent text-black dark:text-white"
-       }`}
+      className={`w-full rounded-2xl p-4 transition duration-200 hover:cursor-pointer
+        ${isActive
+          ? "bg-[#818df8] text-white dark:bg-[#6f58da]" // active state
+          : "bg-transparent text-black dark:text-white hover:bg-[rgba(129,140,248,0.25)]"} 
+        dark:hover:shadow-[0_2px_15px_#6f58da] hover:shadow-[0_2px_15px_#818df8bf]`}
     >
       <p className="font-bold text-left">{communityName}</p>
     </button>
