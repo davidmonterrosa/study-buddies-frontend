e905aa7daf4ce0c5350686d557fa6b0834a10103
AlanBatres1 <alanbatres328@gmail.com>
2025-04-08 10:29:00 -0700

    hamburger fix

--- src/components/HamburgerMyCommunities.tsx
+++ src/components/HamburgerMyCommunities.tsx
@@ -1,22 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
+import ViewCommunityButton from "./ViewCommunityButton";
 
 const MyCommunitiesSidebar = () => {
+    const [activeCommunity, setActiveCommunity] = useState<string | null>(null);
+  
+    // List of communities user is apart of example
+    const communities = ['Spanish', 'French', 'German', 'Italian'];
+
   return (
     <div className="flex flex-col w-64 h-full rounded-r-[15px] bg-white bg-gradient-to-b dark:from-[#271E55] dark:to-[#100B28] p-4 shadow-lg">
       <h1 className="text-center text-2xl font-bold m-4 text-black dark:text-white">My Communities</h1>
       <div className="space-y-4">
-        <button className="w-full rounded-2xl hover:bg-[#818cf88e] p-4 transition-all">
-          <p className="font-bold text-left text-black dark:text-white ">Spanish Learning</p>
-        </button>
-        <button className="w-full rounded-2xl hover:bg-[#818cf88e] p-4 transition-all">
-          <p className="font-bold text-left text-black dark:text-white">French Learning</p>
-        </button>
-        <button className="w-full rounded-2xl hover:bg-[#818cf88e] p-4 transition-all">
-          <p className="font-bold text-left text-black dark:text-white">Math Study Group</p>
-        </button>
-        <button className="w-full rounded-2xl hover:bg-[#818cf88e] p-4 transition-all">
-          <p className="font-bold text-left text-black dark:text-white">Tech Talk</p>
-        </button>
+      <div className='flex flex-col gap-2'>
+        {/* Map of communities */}
+      {communities.map((name) => (
+          <ViewCommunityButton
+            key={name}
+            communityName={name}
+            isActive={activeCommunity === name}
+            onClick={() => setActiveCommunity(name)}
+          />
+        ))}
+      </div>
       </div>
     </div>
   );
