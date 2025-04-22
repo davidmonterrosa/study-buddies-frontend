a429eae785108574f14d69d96e967a21aa9fc5db
davidmonterrosa <dmonterrosa99@gmail.com>
2025-04-22 14:03:26 -0700

    Updated the hamburger menu style

--- src/components/HamburgerMyCommunities.tsx
+++ src/components/HamburgerMyCommunities.tsx
@@ -3,22 +3,16 @@ import React, { useEffect, useState } from "react";
 import ViewCommunityButton from "./ViewCommunityButton";
 import { ICommunityData } from "@/utils/Interfaces/UserInterfaces";
 import { currentUser, getMyCommunities, getToken } from "@/utils/Services/DataServices";
+import { useAppContext } from "@/context/CommunityContext";
+import Link from "next/link";
+import { navigationMenuTriggerStyle } from "./ui/navigation-menu";
 
 const MyCommunitiesSidebar = () => {
     const [activeCommunity, setActiveCommunity] = useState<string | null>(null);
-    const [communityGroups, setcommunityGroups] = useState<ICommunityData[]>([]);
-  
-    useEffect(() => {
-      const fetchMyCommunities = async () => {
-        const loggedInUser = currentUser();
-        if(loggedInUser) {
-          const data = await getMyCommunities(loggedInUser.user.id, getToken());
-          setcommunityGroups(data);
-          console.log(data);
-        }
-      }
-      fetchMyCommunities();
-    }, []);
+    // const [communityGroups, setcommunityGroups] = useState<ICommunityData[]>([]);
+    const {communityGroups} = useAppContext(); 
+
+    console.log("This is the communityGroups:", communityGroups);
   
   return (
     <div className="lg:hidden flex flex-col w-64 h-full rounded-r-[15px] bg-white bg-gradient-to-b dark:from-[#271E55] dark:to-[#100B28] p-4 shadow-lg">
@@ -26,13 +20,14 @@ const MyCommunitiesSidebar = () => {
       <div className="space-y-4">
       <div className='flex flex-col gap-2'>
         {/* Map of communities */}
-      {communityGroups.map((communityGroup: ICommunityData, idx: number) => (
-          <ViewCommunityButton
-            key={idx}
-            communityName={communityGroup.communityName}
-            isActive={activeCommunity === communityGroup.communityName}
-            onClick={() => setActiveCommunity(communityGroup.communityName)}
-          />
+        {communityGroups.map((communityGroup: ICommunityData , idx: number) => (
+          <Link key={idx} href={`/communities/${communityGroup.id}`}>
+              <ViewCommunityButton
+                communityName={communityGroup.communityName}
+                isActive={activeCommunity === communityGroup.communityName}
+                onClick={() => setActiveCommunity(communityGroup.communityName)}
+              />
+          </Link>
         ))}
       </div>
       </div>
