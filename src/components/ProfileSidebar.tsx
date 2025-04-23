8cf1f2b0b3870ab4ef8c35096f2d4121647f9cc6
AlanBatres1 <alanbatres328@gmail.com>
2025-04-23 09:40:15 -0700

    Made the logout button work and removes token

--- src/components/ProfileSidebar.tsx
+++ src/components/ProfileSidebar.tsx
@@ -1,5 +1,6 @@
 "use client";
 import { Drawer, DarkThemeToggle } from "flowbite-react";
+import { useRouter } from "next/navigation"; // 👈 Import useRouter
 
 interface ProfileSidebarProps {
   isOpen: boolean;
@@ -7,17 +8,21 @@ interface ProfileSidebarProps {
 }
 
 const ProfileSidebar: React.FC<ProfileSidebarProps> = ({ isOpen, onClose }) => {
+  const router = useRouter();
+
+  const logoutUser = () => {
+    localStorage.removeItem("Token");
+    router.push("/");
+  };
+
   return (
     <Drawer open={isOpen} onClose={onClose} position="right">
       <div className="flex flex-col mt-4 gap-4 px-4">
         {/* Profile Header (Image + Name + Email) */}
         <div className="flex items-center gap-3">
-          {/* Profile Image */}
           <div className="bg-gradient-to-b from-[#6F58DA] to-[#5131E7] rounded-full w-[60px] h-[60px] flex items-center justify-center">
             <p className="text-[20px] font-bold text-white">AL</p>
           </div>
-
-          {/* Name and Email */}
           <div>
             <p className="font-semibold text-2xl">Ada Lovelace</p>
             <p className="text-xl">alovelace@ucla.edu</p>
@@ -26,7 +31,10 @@ const ProfileSidebar: React.FC<ProfileSidebarProps> = ({ isOpen, onClose }) => {
 
         {/* Buttons Section */}
         <div className="flex flex-col w-full gap-2">
-          <button className="w-full cursor-pointer bg-red-500 text-white py-2 rounded-[10px] font-bold">
+          <button
+            onClick={logoutUser}
+            className="w-full cursor-pointer bg-red-500 text-white py-2 rounded-[10px] font-bold"
+          >
             Logout
           </button>
           <DarkThemeToggle />
