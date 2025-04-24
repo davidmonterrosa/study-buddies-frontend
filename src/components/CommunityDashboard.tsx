55ea376153966d05373d9ed7bd8565cf2a9f7db0
AlanBatres1 <alanbatres328@gmail.com>
2025-04-24 10:27:18 -0700

    Added the dm content into the sessions for now and created the message styling for the direct messages

--- src/components/CommunityDashboard.tsx
+++ src/components/CommunityDashboard.tsx
@@ -5,7 +5,8 @@ import { Tabs, TabsContent, TabsList, TabsTrigger } from './ui/tabs'
 import { Separator } from './ui/separator'
 import { currentUser, getCommunityById, getLoggedInUserData } from '@/utils/Services/DataServices'
 import { CommunityChats, ICommunityData, IUserNameId } from '@/utils/Interfaces/UserInterfaces'
-import BuddiesCompononet from './BuddiesCompononet'
+import BuddiesCompononet from './BuddiesComponent'
+import DirectMessage from './DirectMessage'
 
 interface CommunityDashboardProps {
   communityId: number;
@@ -85,7 +86,6 @@ const CommunityDashboard: React.FC<CommunityDashboardProps> = ({
             </TabsTrigger>
 
             <TabsTrigger
-              disabled
               value="sessionsTab"
               className="relative px-5 py-2 text-2xl font-semibold transition data-[state=active]:text-primary after:content-[''] after:absolute after:bottom-[-10px] after:left-0 after:h-[5px] after:w-full
                         after:rounded-sm data-[state=active]:shadow-none dark:data-[state=active]:bg-transparent dark:data-[state=active]:border-none
@@ -233,116 +233,7 @@ const CommunityDashboard: React.FC<CommunityDashboardProps> = ({
 
           {/* Sessions */}
           <TabsContent value="sessionsTab">
-            <section className="flex flex-col min-h-[calc(0.68*100vh)]   overflow-hidden mt-4">
-              {/* Messages Area */}
-
-              {/* Time Stamp */}
-              <div className="flex justify-center pb-4">
-                <div className="bg-[#F3F3F3] dark:bg-[#140D34] dark:border-[1px] dark:border-[#aa7dfc40] rounded-[10px] py-[2px] px-[5px] max-h-10  flex items-center justify-center">
-                  <p className="text-center font-bold">4/22/25</p>
-                </div>
-              </div>
-
-              <div className="flex-1 md:px-4 overflow-y-auto space-y-3">
-                {/* Example received message */}
-                <div className="flex items-start gap-2">
-                  <div className="bg-[#F6F6F6] dark:bg-[#140D34] dark:border-[1px] dark:border-[#aa7dfc40] px-3 py-2 rounded-lg w-full text-sm">
-                    <div className="flex mt-2 gap-2 items-center">
-                      <div className="bg-[#818CF8] rounded-full w-[30px] h-[30px] flex items-center justify-center">
-                        <p className="text-[14px] font-bold text-black">ML</p>
-                      </div>
-                      <p className="font-semibold text-sm">Maria lopez</p>
-                      <p>3:45 PM</p>
-                    </div>
-                    <p>
-                      Hola amigos! I'm organizing a study session for tomorrow
-                      at 7 PM to review verb conjugations. Anyone interested in
-                      joining?
-                    </p>
-                    {/* Likes and Reply */}
-                    <div className="flex row justify-end gap-3">
-                      {/* Likes */}
-                      <div className="flex row items-center gap-1">
-                        <p>12</p>
-                        <div className="bg-[#818CF8] rounded-full w-[30px] h-[30px] flex items-center justify-center">
-                          <p className="text-[14px] font-bold text-black">ML</p>
-                        </div>
-                      </div>
-                      {/* Reply */}
-                      <div className="flex row items-center gap-1">
-                        <p>2</p>
-                        <div className="bg-[#818CF8] rounded-full w-[30px] h-[30px] flex items-center justify-center">
-                          <p className="text-[14px] font-bold text-black">ML</p>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-
-                {/* Example sent message */}
-                <div className="flex justify-end">
-                  <div className="bg-[#CBD0FF] dark:bg-[#3D3179] dark:border-[1px] dark:border-[#aa7dfc40] px-3 py-2 rounded-lg w-full text-sm">
-                    <div className="flex mt-2 gap-2 items-center">
-                      <div className="bg-[#3730A3] rounded-full w-[30px] h-[30px] flex items-center justify-center">
-                        <p className="text-[14px] font-bold text-white">AL</p>
-                      </div>
-                      <p className="font-semibold text-sm">You</p>
-                      <p>6:55 PM</p>
-                    </div>
-                    <p>
-                      Just found this amazing resource for Spanish vocabulary:
-                      link.com/spanish-vocab. It has flashcards and spaced
-                      repetition features!
-                    </p>
-                    {/* Likes and Reply */}
-                    <div className="flex row justify-end gap-3">
-                      {/* Likes */}
-                      <div className="flex row items-center gap-1">
-                        <p>12</p>
-                        <div className="bg-[#3730A3] rounded-full w-[30px] h-[30px] flex items-center justify-center">
-                          <p className="text-[14px] font-bold text-white">ML</p>
-                        </div>
-                      </div>
-                      {/* Reply */}
-                      <div className="flex row items-center gap-1">
-                        <p>2</p>
-                        <div className="bg-[#3730A3] rounded-full w-[30px] h-[30px] flex items-center justify-center">
-                          <p className="text-[14px] font-bold text-white">ML</p>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-
-              {/* Message Input Bar */}
-              <div className="p-3 flex items-center w-full">
-                <div className="relative flex-1">
-                  <input
-                    type="text"
-                    placeholder="Type your message..."
-                    className="w-full pr-10 px-4 py-2 rounded-[15px] drop-shadow-[0_1px_2px_rgba(0,0,0,0.25)] text-sm bg-[#F6F6F6] dark:bg-transparent dark:border-[1px] dark:border-[#aa7dfc40] focus:outline-none focus:ring-2 focus:ring-[#818CF8] dark:focus:ring-[#a97dfc96]"
-                  />
-                  {/* Send Icon */}
-                  <button className="absolute right-2 top-1/2 -translate-y-1/2 cursor-pointer">
-                    <svg width="27" height="27" viewBox="0 0 27 27" xmlns="http://www.w3.org/2000/svg" className="transition-colors duration-300">
-                      {/* Define the gradient */}
-                      <defs>
-                        <linearGradient id="darkGradient" x1="0" y1="0" x2="0" y2="1" >
-                          <stop offset="0%" stopColor="#6F58DA" />
-                          <stop offset="100%" stopColor="#5131E7" />
-                        </linearGradient>
-                      </defs>
-
-                      <path d="M3.9696 3.01198L23.6529 12.4257C24.2346 12.704 24.4807 13.4011 24.2025 13.9829C24.0874 14.2235 23.8935 14.4174 23.6529 14.5325L3.9696 23.9462C3.38783 24.2245 2.69066 23.9784 2.41242 23.3966C2.29151 23.1438 2.26555 22.856 2.33929 22.5856L4.30801 15.3668C4.35733 15.1859 4.51 15.0521 4.69575 15.0268L14.2337 13.7312C14.3159 13.7195 14.384 13.6655 14.4155 13.5917L14.4319 13.5331C14.4475 13.4236 14.3846 13.3211 14.2859 13.2825L14.2337 13.2689L4.70691 11.9733C4.52118 11.9481 4.36855 11.8142 4.31924 11.6334L2.33929 4.3726C2.16961 3.75044 2.53641 3.10853 3.15857 2.93885C3.42893 2.86512 3.71679 2.89107 3.9696 3.01198Z"
-                        fill="currentColor"
-                        className="text-[#818CF8] dark:fill-[url(#darkGradient)]"
-                      />
-                    </svg>
-                  </button>
-                </div>
-              </div>
-            </section>
+            <DirectMessage/>
           </TabsContent>
 
           {/* Buddies */}
