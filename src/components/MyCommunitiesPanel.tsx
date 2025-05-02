9da3157b1facb712dbc4bd678bec2997753018fd
AlanBatres1 <alanbatres328@gmail.com>
2025-05-02 11:09:31 -0700

    fixed the height

--- src/components/MyCommunitiesPanel.tsx
+++ src/components/MyCommunitiesPanel.tsx
@@ -25,7 +25,7 @@ const MyCommunitiesPanel = () => {
   }, []);
 
   return (
-    <aside className="hidden lg:flex flex-col w-1/5 shadow-[0_0px_5px_rgba(0,0,0,0.25)] dark:bg-gradient-to-b dark:from-[#271E55] dark:to-[#100B28] dark:border-[2px] dark:border-[#aa7dfc40] rounded-lg overflow-hidden max-h-[calc(0.90*100vh-1rem)]">
+    <aside className="hidden lg:flex flex-col w-1/5 shadow-[0_0px_5px_rgba(0,0,0,0.25)] dark:bg-gradient-to-b dark:from-[#271E55] dark:to-[#100B28] dark:border-[2px] dark:border-[#aa7dfc40] rounded-lg overflow-hidden h-[calc(0.90*100vh-1rem)]">
 
       {/* Scrollable inner content */}
       <div className="flex-1 overflow-y-auto scrollbar p-3 space-y-4">
